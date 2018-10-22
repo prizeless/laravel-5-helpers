74c6bbefca84cef286db42309df1f036843bde55
Antony Masocha <antony@prizeless.net>
2018-10-22 12:18:45 +0200

    Add search array

--- src/Uuid/UuidModel.php
+++ src/Uuid/UuidModel.php
@@ -12,16 +12,26 @@ class UuidModel extends Model
 
     public function scopeLike($query, $field, $value)
     {
+        if (is_array($value) === true) {
+            return $query->whereIn($field, $value);
+        }
+
         return $query->where($field, 'LIKE', "%$value%");
     }
 
     public function scopeORWhereWildCard($query, $field, $value)
     {
+        if (is_array($value) === true) {
+            return $query->orWhereIn($field, $value);
+        }
         return $query->orWhere($field, 'LIKE', "%$value%");
     }
 
     public function scopeWhereWildCard($query, $field, $value)
     {
+        if (is_array($value) === true) {
+            return $query->whereIn($field, $value);
+        }
         return $query->where($field, 'LIKE', "%$value%");
     }
 }
